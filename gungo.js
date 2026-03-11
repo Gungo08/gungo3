488bffb91d535159bb6d21845a049db67f9fdb32
gungotv <gungorey63@gmail.com>
2026-03-11 03:40:01 -0400

    Update print statement from 'Hello' to 'Goodbye'

--- gungo.js
+++ gungo.js
@@ -5,7 +5,7 @@
 
     // 1. INICIALIZACIÓN DE FIREBASE
     var firebaseConfig = {
-        apiKey: "AIzaSyBv849w6NNk_4QhOnaY3x7LOE38apvc6o4",
+        apiKey: "AIzaSyCumX5UWoZEgxz7iOOOR9OXQGG2YWDX7ik" ,
         authDomain: "gungo-tv.firebaseapp.com",
         projectId: "gungo-tv",
         storageBucket: "gungo-tv.firebasestorage.app",
@@ -892,5 +892,6 @@
     document.addEventListener("DOMContentLoaded", () => {
         window.checkCookieConsent();
     });
+
 
 })(); // <-- CIERRE DEL ESCUDO DE SEGURIDAD IIFE
